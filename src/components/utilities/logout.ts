a547de21925b17aed4e6b41bba93d111bfc94fbc
Mike Nguyen <mikenguyen7268@gmail.com>
2021-07-04 15:06:30 -0500

    Remove unnecessary files

--- src/components/utilities/logout.ts
+++
@@ -1,8 +0,0 @@
-import { History } from "history";
-import { AUTH_KEY, REFRESH_AUTH_KEY } from "../../constants";
-
-export const logout = (history: History) => {
-    localStorage.removeItem(AUTH_KEY);
-    localStorage.removeItem(REFRESH_AUTH_KEY);
-    history.push("/login");
-};
