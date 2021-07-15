bc6a146814036f879d0e1d2e035cbce9588f59ea
Mike Nguyen <mikenguyen7268@gmail.com>
2021-07-15 12:31:50 -0500

    Redo Footer to prevent misunderstaning that this is a real company

--- src/components/login/Footer.tsx
+++ src/components/login/Footer.tsx
@@ -1,31 +1,31 @@
 const Footer = () => {
     return (
         <div className="p-10 flex flex-col gap-y-4 md:flex-row justify-between items-center border-t">
-            <h1 className="text-xl">Quiz Ltd.</h1>
+            <h1 className="text-xl">Technologies used in this project</h1>
             <div className="flex gap-x-4">
                 <button
                     type="button"
                     className="text-blue-700 hover:text-blue-900"
                 >
-                    Terms
+                    TypeScript
                 </button>
                 <button
                     type="button"
                     className="text-blue-700 hover:text-blue-900"
                 >
-                    Privacy
+                    GraphQL
                 </button>
                 <button
                     type="button"
                     className="text-blue-700 hover:text-blue-900"
                 >
-                    Security
+                    React
                 </button>
                 <button
                     type="button"
                     className="text-blue-700 hover:text-blue-900"
                 >
-                    Get in touch
+                    TailwindCSS
                 </button>
             </div>
         </div>
