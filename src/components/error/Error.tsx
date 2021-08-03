72fb58d1cb19b6df10a7bf72a3009a21b35bf643
Mike Nguyen <mikenguyen7268@gmail.com>
2021-08-03 15:51:03 -0500

    Added error handling case when user accessing restricted information

--- src/components/error/Error.tsx
+++ src/components/error/Error.tsx
@@ -1,6 +1,7 @@
 import { ApolloError } from "@apollo/client";
 import { Redirect } from "react-router-dom";
 import { logout } from "../utilities/logout";
+import { FORBIDDEN, RESOURCE_NOT_FOUND } from "./errorCode";
 
 interface Props {
     error: ApolloError;
@@ -17,11 +18,16 @@ const ErrorComponent: React.FC<Props> = ({ error }) => {
         return <Redirect to="/login" />;
     } else if (
         error.graphQLErrors.filter(
-            (errorObject) =>
-                errorObject.extensions!.code === "RESOURCE_NOT_FOUND"
+            (errorObject) => errorObject.extensions!.code === RESOURCE_NOT_FOUND
         ).length > 0
     )
         return <div>Oops, what you seems to be looking for does not exist</div>;
+    else if (
+        error.graphQLErrors.filter(
+            (errorObject) => errorObject.extensions!.code === FORBIDDEN
+        ).length > 0
+    )
+        return <div>Oops, you are not allowed to access this information</div>;
     console.log(error.graphQLErrors);
 
     return <div></div>;
